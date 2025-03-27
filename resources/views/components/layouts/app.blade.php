931701e828a67a8356e101c96ab388163be1f602
rigili27 <rigili27@gmail.com>
2025-03-27 16:52:35 -0300

    Primer commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        {{-- @filamentStyles --}}
+        @livewireStyles
+
+
+
+    </head>
+    <body class="font-sans text-gray-900 antialiased bg-gray-400">
+        
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+
+        {{-- @livewire('notifications') --}}
+        {{-- @filamentScripts --}}
+        @livewireScripts
+        {{-- <script src="./node_modules/preline/dist/preline.js"></script> --}}
+    </body>
+</html>
