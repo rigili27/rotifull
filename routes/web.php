0047e464cd0eb78f153eea7c47e29d07d838eab1
rigili27 <rigili27@gmail.com>
2025-05-31 19:48:02 -0300

    ohter

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\TeamBuilder;
 use App\Livewire\Home;
 use App\Livewire\TeamEditor;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,8 @@ Route::get('/', function () {
 
 Route::get('/home', Home::class)->name('home');
 Route::get('/cart', Cart::class)->name('cart');
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]);
+});
